695cf4c3cb3594d700df4b3007f7cd896f553434
qwetuo <chewminzhuang@hotmail.com>
2018-08-20 21:08:47 +0800

    remove utils fns that have been moved to Rover class + add test for getMaxX&Y

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,15 +1,20 @@
 const {
+  getMaxX,
+  getMaxY,
   getInitialDirection,
   getInitialX,
-  getInitialY,
-  changeDirectionToLeft,
-  moveN,
-  moveS,
-  moveE,
-  moveW,
-  getFinalPosition,
-  changeDirectionToRight
+  getInitialY
 } = require("../utils");
+
+test("getMaxX should return the highest x coordinate possible", () => {
+  expect(getMaxX("5 5")).toEqual("5");
+  expect(getMaxX("10 6")).toEqual("10");
+});
+
+test("getMaxY should return the highest y coordinate possible", () => {
+  expect(getMaxY("5 5")).toEqual("5");
+  expect(getMaxY("10 12")).toEqual("12");
+});
 
 test("getInitialDirection should return only the direction of the rover when provided its initial position", () => {
   expect(getInitialDirection("1 2 N")).toEqual("N");
@@ -27,46 +32,4 @@ test("getInitialY should return the y-coordinate/2nd position of the rover when 
   expect(getInitialY("1 2 N")).toEqual("2");
   expect(getInitialY("11 22 E")).toEqual("22");
   expect(getInitialY("1 22 S")).toEqual("22");
-});
-
-test("changeDirectionToLeft should return the direction 90 degrees left from provided input", () => {
-  expect(changeDirectionToLeft("E")).toEqual("N");
-  expect(changeDirectionToLeft("S")).toEqual("E");
-  expect(changeDirectionToLeft("W")).toEqual("S");
-  expect(changeDirectionToLeft("N")).toEqual("W");
-});
-
-test("changeDirectionToRight should return the direction 90 degrees right from provided input", () => {
-  expect(changeDirectionToRight("N")).toEqual("E");
-  expect(changeDirectionToRight("E")).toEqual("S");
-  expect(changeDirectionToRight("S")).toEqual("W");
-  expect(changeDirectionToRight("W")).toEqual("N");
-});
-
-test("moveN should return new y coordinate from a move towards North", () => {
-  expect(moveN("0")).toEqual("1");
-});
-
-test("moveS should return new y coordinate from a move towards South", () => {
-  expect(moveS("1")).toEqual("0");
-});
-
-test("moveE should return new x coordinate from a move towards East", () => {
-  expect(moveE("0")).toEqual("1");
-});
-
-test("moveW should return new x coordinate from a move towards West", () => {
-  expect(moveW("1")).toEqual("0");
-});
-
-test("getFinalPosition should return final position of Rover after moving according to the instructions", () => {
-  const instructions = "LMLMLMLMM";
-  const initialX = "1";
-  const initialY = "2";
-  const initialDirection = "N";
-  expect(getFinalPosition("MMM", "0", "0", "N")).toEqual("0 3 N");
-  expect(
-    getFinalPosition(instructions, initialX, initialY, initialDirection)
-  ).toEqual("1 3 N");
-  expect(getFinalPosition("MMRMMRMRRM", "3", "3", "E")).toEqual("5 1 E");
 });
--- utils.js
+++ utils.js
@@ -18,73 +18,10 @@ const getInitialY = INITIAL_POS => {
   return INITIAL_POS.split(" ")[1];
 };
 
-const changeDirectionToLeft = currentDirection => {
-  if (currentDirection === "N") return "W";
-  const compass = ["N", "E", "S", "W"];
-  const currIndex = compass.indexOf(currentDirection);
-  return compass[currIndex - 1];
-};
-
-const changeDirectionToRight = currentDirection => {
-  if (currentDirection === "W") return "N";
-  const compass = ["N", "E", "S", "W"];
-  const currIndex = compass.indexOf(currentDirection);
-  return compass[currIndex + 1];
-};
-
-const moveN = yPosition => {
-  return (+yPosition + 1).toString();
-};
-
-const moveS = yPosition => {
-  return (+yPosition - 1).toString();
-};
-
-const moveE = xPosition => {
-  return (+xPosition + 1).toString();
-};
-
-const moveW = xPosition => {
-  return (+xPosition - 1).toString();
-};
-
-const getFinalPosition = (
-  instructions,
-  initialX,
-  initialY,
-  initialDirection
-) => {
-  let currentX = initialX;
-  let currentY = initialY;
-  let currentDirection = initialDirection;
-  instructionsArr = instructions.split("");
-  instructionsArr.forEach(instruction => {
-    if (instruction === "L")
-      currentDirection = changeDirectionToLeft(currentDirection);
-    if (instruction === "R")
-      currentDirection = changeDirectionToRight(currentDirection);
-    if (instruction === "M") {
-      if (currentDirection === "N") currentY = moveN(currentY);
-      if (currentDirection === "S") currentY = moveS(currentY);
-      if (currentDirection === "E") currentX = moveE(currentX);
-      if (currentDirection === "W") currentX = moveW(currentX);
-    }
-  });
-  const result = currentX + " " + currentY + " " + currentDirection;
-  return result;
-};
-
 module.exports = {
   getMaxX,
   getMaxY,
   getInitialDirection,
   getInitialX,
-  getInitialY,
-  changeDirectionToLeft,
-  moveN,
-  moveS,
-  moveE,
-  moveW,
-  getFinalPosition,
-  changeDirectionToRight
+  getInitialY
 };
