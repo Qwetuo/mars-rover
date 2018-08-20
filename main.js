1cd93fdbacd1319a9764b279dba9f06825109993
qwetuo <chewminzhuang@hotmail.com>
2018-08-20 14:26:06 +0800

    Renamed variable names sensibly

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 const { getUserInput, rl } = require("./readline-helper");
 
 const main = async () => {
-  const x = await getUserInput(
+  const PLATAEU_SIZE = await getUserInput(
     "Specify the size of the Mars plateau (e.g. 5 5):"
   );
-  console.log("x is", x);
+  console.log("Plataeu size is", PLATAEU_SIZE);
 
-  const y = await getUserInput(
+  const INITIAL_POS = await getUserInput(
     "Specify the initial coordinates and direction of the mars rover (e.g. 1 2 N):"
   );
-  console.log("y is", y);
+  console.log("Rover Initial Position  is", INITIAL_POS);
 
-  const z = await getUserInput(
+  const INSTRUCTIONS = await getUserInput(
     "Specify the instructions for the mars rover (e.g. LMLMLMLMM):"
   );
-  console.log("z is", z);
+  console.log("Move Instructions for Rover is", INSTRUCTIONS);
 
   // TODO: include the functions that you've implemented for this kata
 
